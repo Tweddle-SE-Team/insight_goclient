04586806effd74884d2ff7f3ff93e48ec4117cf2
Daniel Isaac Khan Ramiro <dkhanram@cisco.com>
2017-09-21 17:34:55 -0700

    Add support for tags

---
+++ tags.go
@@ -0,0 +1,137 @@
+package logentries_goclient
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Tags struct {
+	client *client `json:"-"`
+}
+
+func NewTags(c *client) Tags {
+	return Tags{c}
+}
+
+// Structs meant for clients
+type PostTag struct {
+	Type     string      `json:"type"`
+	Name     string      `json:"name"`
+	Sources  PostSources `json:"sources"`
+	Actions  PostActions `json:"actions"`
+	Patterns []string    `json:"patters"`
+	Labels   Labels      `json:"labels"`
+}
+
+type Tag struct {
+	Id      string   `json:"id"`
+	Type    string   `json:"type"`
+	Name    string   `json:"name"`
+	Sources Sources  `json:"sources"`
+	Actions Actions  `json:"actions"`
+	Patters []string `json:"patters"`
+	Labels  Labels   `json:"labels"`
+}
+
+type PostSource struct {
+	Id string `json:"id"`
+}
+
+type Source struct {
+	Id              string `json:"id"`
+	Name            string `json:"name"`
+	RetentionPeriod string `json:"retention_period"`
+	StoredDays      []int  `json:"stored_days"`
+}
+
+type PostAction struct {
+	MinMatchesCount  int               `json:"min_matches_count"`
+	MinReportCount   int               `json:"min_report_count"`
+	MinMatchesPeriod string            `json:"min_matches_period"`
+	MinReportPeriod  string            `json:"min_report_period"`
+	Targets          Targets           `json:"targets"`
+	AlertContentSet  map[string]string `json:"alert_content_set"`
+	Enabled          bool              `json:"enabled"`
+	Type             string            `json:"type"`
+}
+
+type Action struct {
+	Id               string            `json:"id"`
+	MinMatchesCount  int               `json:"min_matches_count"`
+	MinReportCount   int               `json:"min_report_count"`
+	MinMatchesPeriod string            `json:"min_matches_period"`
+	MinReportPeriod  string            `json:"min_report_period"`
+	Targets          Targets           `json:"targets"`
+	AlertContentSet  map[string]string `json:"alert_content_set"`
+	Enabled          bool              `json:"enabled"`
+	Type             string            `json:"type"`
+}
+
+type Target struct {
+	Id        string    `json:"id"`
+	Type      string    `json:"type"`
+	ParamsSet ParamsSet `json:"params_set"`
+}
+
+type ParamsSet struct {
+	Direct string `json:"direct"`
+	Teams  string `json:"teams"`
+	Users  string `json:"users"`
+}
+
+type Label struct {
+	Id       string `json:"id"`
+	SN       int    `json:"sn"`
+	Name     string `json:"name"`
+	Color    string `json:"color"`
+	Reserved bool   `json:"reserved"`
+}
+
+type Sources []Source
+type PostSources []PostSource
+type Actions []Action
+type PostActions []PostAction
+type Labels []Label
+type Targets []Target
+
+// Structs meant for marshalling/un-marshalling purposes
+type tagsCollection struct {
+	Tags []Tag `json:"tags"`
+}
+
+type getTag struct {
+	Tag Tag `json:"tag"`
+}
+
+func (t *Tags) getPath() string {
+	return "management/tags"
+}
+
+func (t *Tags) GetTags() ([]Tag, error) {
+	tags := &tagsCollection{}
+	if err := t.client.get(t.getPath(), tags); err != nil {
+		return nil, err
+	}
+	return tags.Tags, nil
+}
+
+func (t *Tags) GetTag(id string) (Tag, error) {
+	if id == "" {
+		return Tag{}, errors.New("missing tag id")
+	}
+
+	tagEndPoint := fmt.Sprintf("%s/%s", t.getPath(), id)
+	tag := &getTag{}
+	if err := t.client.get(tagEndPoint, tag); err != nil {
+		return Tag{}, err
+	}
+	return tag.Tag, nil
+}
+
+func (t *Tags) PostTag(postTag PostTag) (Tag, error) {
+	tag := &getTag{}
+	if err := t.client.post(t.getPath(), postTag, tag); err != nil {
+		return Tag{}, err
+	}
+	return tag.Tag, nil
+}
---
+++ tags_test.go
@@ -0,0 +1,241 @@
+package logentries_goclient
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"fmt"
+
+	"github.com/dikhan/logentries_goclient/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTags_GetTags(t *testing.T) {
+
+	expectedTags := []Tag{
+		{
+			Id:   "tag-uuid",
+			Name: "Login Failure",
+			Type: "Alert",
+			Sources: []Source{
+				{
+					Id:              "source-uuid",
+					Name:            "auth.log",
+					RetentionPeriod: "default",
+					StoredDays:      []int{},
+				},
+			},
+			Actions: []Action{
+				{
+					Id:               "action-uuid",
+					MinMatchesCount:  1,
+					MinReportCount:   1,
+					MinMatchesPeriod: "Day",
+					MinReportPeriod:  "Day",
+					Targets: Targets{
+						{
+							Id:   "",
+							Type: "",
+							ParamsSet: ParamsSet{
+								Direct: "user@example.com",
+								Teams:  "some-team",
+								Users:  "user@example.com",
+							},
+						},
+					},
+					AlertContentSet: map[string]string{},
+					Enabled:         true,
+					Type:            "Alert",
+				},
+			},
+			Labels: Labels{
+				{
+					Id:       "label-uuid",
+					Name:     "Login Failure",
+					Reserved: false,
+					Color:    "007afb",
+					SN:       1056,
+				},
+			},
+			Patters: []string{"Power Button as"},
+		},
+	}
+
+	requestMatcher := testutils.NewRequestMatcher(http.MethodGet, "/management/tags", nil, http.StatusOK, &tagsCollection{expectedTags})
+	tags := getTagsClient(requestMatcher)
+
+	returnedTags, err := tags.GetTags()
+	assert.Nil(t, err)
+	assert.True(t, reflect.DeepEqual(expectedTags, returnedTags))
+}
+
+func TestTags_GetTag(t *testing.T) {
+
+	expectedTag := Tag{
+		Id:   "tag-uuid",
+		Name: "Login Failure",
+		Type: "Alert",
+		Sources: []Source{
+			{
+				Id:              "source-uuid",
+				Name:            "auth.log",
+				RetentionPeriod: "default",
+				StoredDays:      []int{},
+			},
+		},
+		Actions: []Action{
+			{
+				Id:               "action-uuid",
+				MinMatchesCount:  1,
+				MinReportCount:   1,
+				MinMatchesPeriod: "Day",
+				MinReportPeriod:  "Day",
+				Targets: Targets{
+					{
+						Id:   "",
+						Type: "",
+						ParamsSet: ParamsSet{
+							Direct: "user@example.com",
+							Teams:  "some-team",
+							Users:  "user@example.com",
+						},
+					},
+				},
+				AlertContentSet: map[string]string{},
+				Enabled:         true,
+				Type:            "Alert",
+			},
+		},
+		Labels: Labels{
+			{
+				Id:       "label-uuid",
+				Name:     "Login Failure",
+				Reserved: false,
+				Color:    "007afb",
+				SN:       1056,
+			},
+		},
+		Patters: []string{"Power Button as"},
+	}
+
+	url := fmt.Sprintf("/management/tags/%s", expectedTag.Id)
+	requestMatcher := testutils.NewRequestMatcher(http.MethodGet, url, nil, http.StatusOK, &getTag{expectedTag})
+
+	tags := getTagsClient(requestMatcher)
+
+	returnedTag, err := tags.GetTag(expectedTag.Id)
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedTag, returnedTag)
+
+}
+
+func TestTags_GetTagSetErrorsIfTagIdIsEmpty(t *testing.T) {
+	tags := Tags{nil}
+	_, err := tags.GetTag("")
+	assert.NotNil(t, err)
+	assert.Error(t, err, "missing log logSetId")
+}
+
+func TestTags_PostTag(t *testing.T) {
+
+	postTag := PostTag{
+		Name: "Foo Bar Tag",
+		Type: "Alert",
+		Sources: []PostSource{
+			{
+				Id: "source-uuid",
+			},
+		},
+		Actions: []PostAction{
+			{
+				MinMatchesCount:  1,
+				MinReportCount:   1,
+				MinMatchesPeriod: "Day",
+				MinReportPeriod:  "Day",
+				Targets: Targets{
+					{
+						Type: "mailto",
+						ParamsSet: ParamsSet{
+							Direct: "test@test.com",
+						},
+					},
+				},
+				AlertContentSet: map[string]string{"le_context": "true"},
+				Enabled:         true,
+				Type:            "Alert",
+			},
+		},
+		Labels: Labels{
+			{
+				Id:       "label-uuid",
+				Name:     "Login Failure",
+				Reserved: false,
+				Color:    "007afb",
+				SN:       1056,
+			},
+		},
+		Patterns: []string{"/Foo Bar/"},
+	}
+
+	expectedTag := Tag{
+		Id:   "new-tag-uuid",
+		Name: postTag.Name,
+		Type: postTag.Type,
+		Sources: []Source{
+			{
+				Id:              postTag.Sources[0].Id,
+				Name:            "auth.log",
+				RetentionPeriod: "default",
+				StoredDays:      []int{},
+			},
+		},
+		Actions: []Action{
+			{
+				Id:               "new-action-uuid",
+				MinMatchesCount:  postTag.Actions[0].MinMatchesCount,
+				MinReportCount:   postTag.Actions[0].MinReportCount,
+				MinMatchesPeriod: postTag.Actions[0].MinMatchesPeriod,
+				MinReportPeriod:  postTag.Actions[0].MinReportPeriod,
+				Targets: Targets{
+					{
+						Id:   "new-target-uuid",
+						Type: postTag.Actions[0].Targets[0].Type,
+						ParamsSet: ParamsSet{
+							Direct: postTag.Actions[0].Targets[0].ParamsSet.Direct,
+							Teams:  postTag.Actions[0].Targets[0].ParamsSet.Teams,
+							Users:  postTag.Actions[0].Targets[0].ParamsSet.Users,
+						},
+					},
+				},
+				AlertContentSet: postTag.Actions[0].AlertContentSet,
+				Enabled:         postTag.Actions[0].Enabled,
+				Type:            postTag.Actions[0].Type,
+			},
+		},
+		Labels: Labels{
+			{
+				Id:       postTag.Labels[0].Id,
+				Name:     postTag.Labels[0].Name,
+				Reserved: postTag.Labels[0].Reserved,
+				Color:    postTag.Labels[0].Color,
+				SN:       postTag.Labels[0].SN,
+			},
+		},
+		Patters: postTag.Patterns,
+	}
+
+	requestMatcher := testutils.NewRequestMatcher(http.MethodPost, "/management/tags", postTag, http.StatusCreated, &getTag{expectedTag})
+
+	tags := getTagsClient(requestMatcher)
+
+	returnedTag, err := tags.PostTag(postTag)
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedTag, returnedTag)
+
+}
+
+func getTagsClient(requestMatcher testutils.TestRequestMatcher) Tags {
+	c := getTestClient(requestMatcher)
+	return NewTags(c)
+}
