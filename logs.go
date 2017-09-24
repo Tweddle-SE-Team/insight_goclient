fee61d31097f2cf1575d202c3ad8549b02a69985
Daniel Isaac Khan Ramiro <di.khan.r@gmail.com>
2017-09-23 19:19:24 -0700

    Add support for Log remaining CRUD operations
    - Refactored logSetEndPoint into its own method to reuse the code
    - Code reorganisation
    - Fixed tests having the matcher expect the wrapper structs instead

--- logs.go
+++ logs.go
@@ -1,9 +1,22 @@
 package logentries_goclient
 
+import (
+	"errors"
+	"fmt"
+)
+
+// The Logs resource allows you to interact with Logs in your account. The following operations are supported:
+// - Get details of an existing Log
+// - Get details of a list of all Logs
+// - Create a new Log
+// - Update an existing Log
+// - Delete a Log
+
 type Logs struct {
 	client *client `json:"-"`
 }
 
+// NewLogs creates a new Logs struct that exposes CRUD operations
 func NewLogs(c *client) Logs {
 	return Logs{c}
 }
@@ -15,7 +28,7 @@ type Log struct {
 	Name        string       `json:"name"`
 	LogsetsInfo []LogInfo `json:"logsets_info"`
 	UserData    LogUserData  `json:"user_data"`
-	Token       []string     `json:"token"`
+	Tokens       []string     `json:"tokens"`
 	SourceType  string       `json:"source_type"`
 	TokenSeed   string       `json:"token_seed"`
 	Structures  []string     `json:"structures"`
@@ -26,20 +39,110 @@ type LogUserData struct {
 	LogEntriesAgentFollow   string `json:"le_agent_follow"`
 }
 
+type PostLog struct {
+	Name        string           `json:"name"`
+	LogsetsInfo []PostLogSetInfo `json:"logsets_info"`
+	UserData    LogUserData      `json:"user_data"`
+	Tokens       []string         `json:"tokens"`
+	SourceType  string           `json:"source_type"`
+	TokenSeed   string           `json:"token_seed"`
+	Structures  []string         `json:"structures"`
+}
+
+type PutLog struct {
+	Name        string           `json:"name"`
+	LogsetsInfo []LogSetInfo `json:"logsets_info"`
+	UserData    LogUserData      `json:"user_data"`
+	Tokens       []string         `json:"tokens"`
+	SourceType  string           `json:"source_type"`
+	TokenSeed   string           `json:"token_seed"`
+	Structures  []string         `json:"structures"`
+}
+
+type LogInfo struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Links []Link `json:"links"`
+}
+
 // Structs meant for marshalling/un-marshalling purposes
 
 type logsCollection struct {
 	Logs []Log
 }
 
-func (l *Logs) getPath() string {
-	return "management/logs"
+type getLog struct {
+	Log Log `json:"log"`
 }
 
+type postLog struct {
+	PostLog PostLog `json:"log"`
+}
+
+type putLog struct {
+	PutLog PutLog `json:"log"`
+}
+
+// GetLogs lists all Logs for an account
 func (l *Logs) GetLogs() ([]Log, error) {
 	logs := &logsCollection{}
 	if err := l.client.get(l.getPath(), logs); err != nil {
 		return nil, err
 	}
 	return logs.Logs, nil
+}
+
+// GetLog gets a specific Log from an account
+func (l *Logs) GetLog(logId string) (Log, error) {
+	if logId == "" {
+		return Log{}, errors.New("logId input parameter is mandatory")
+	}
+	log := &getLog{}
+	if err := l.client.get(l.getLogEndPoint(logId), log); err != nil {
+		return Log{}, err
+	}
+	return log.Log, nil
+}
+
+// PostLog adds a log to a given account.
+func (l *Logs) PostLog(p PostLog) (Log, error) {
+	logSet := &getLog{}
+	postLog := &postLog{p}
+	if err := l.client.post(l.getPath(), postLog, logSet); err != nil {
+		return Log{}, err
+	}
+	return logSet.Log, nil
+}
+
+// PutLog updates a specific Log for an account
+func (l *Logs) PutLog(logId string, p PutLog) (Log, error) {
+	if logId == "" {
+		return Log{}, errors.New("logId input parameter is mandatory")
+	}
+	log := &getLog{}
+	putLogSet := &putLog{p}
+	if err := l.client.put(l.getLogEndPoint(logId), putLogSet, log); err != nil {
+		return Log{}, err
+	}
+	return log.Log, nil
+}
+
+// DeleteLog deletes a specific Log from an account.
+func (l *Logs) DeleteLog(logId string) error {
+	if logId == "" {
+		return errors.New("logId input parameter is mandatory")
+	}
+	var err error
+	if err = l.client.delete(l.getLogEndPoint(logId)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *Logs) getPath() string {
+	return "management/logs"
+}
+
+func (l *Logs) getLogEndPoint(logId string) string {
+	return fmt.Sprintf("%s/%s", l.getPath(), logId)
 }
--- logs_test.go
+++ logs_test.go
@@ -7,13 +7,14 @@ import (
 
 	"github.com/dikhan/logentries_goclient/testutils"
 	"github.com/stretchr/testify/assert"
+	"fmt"
 )
 
 func TestLogs_GetLogs(t *testing.T) {
 
 	expectedLogs := []Log{
 		{
-			Id:   "log-set-uuid",
+			Id:   "log-uuid",
 			Name: "MyLogSet",
 			LogsetsInfo: []LogInfo{
 				{
@@ -27,7 +28,7 @@ func TestLogs_GetLogs(t *testing.T) {
 					},
 				},
 			},
-			Token:      []string{},
+			Tokens:      []string{},
 			SourceType: "AGENT",
 			TokenSeed:  "",
 			Structures: []string{},
@@ -44,6 +45,172 @@ func TestLogs_GetLogs(t *testing.T) {
 	returnedLogs, err := logs.GetLogs()
 	assert.Nil(t, err)
 	assert.True(t, reflect.DeepEqual(expectedLogs, returnedLogs))
+}
+
+func TestLogs_GetLog(t *testing.T) {
+
+	expectedLog := Log{
+			Id:   "log-uuid",
+			Name: "MyLogSet",
+			LogsetsInfo: []LogInfo{
+				{
+					Id:   "log-set-uuid",
+					Name: "MyLogSet",
+					Links: []Link{
+						{
+							Href: "https://rest.logentries.com/management/logsets/log-set-uuid",
+							Rel:  "self",
+						},
+					},
+				},
+			},
+			Tokens:      []string{},
+			SourceType: "AGENT",
+			TokenSeed:  "",
+			Structures: []string{},
+			UserData: LogUserData{
+				LogEntriesAgentFileName: "",
+				LogEntriesAgentFollow:   "",
+			},
+	}
+
+	url := fmt.Sprintf("/management/logs/%s", expectedLog.Id)
+	requestMatcher := testutils.NewRequestMatcher(http.MethodGet, url, nil, http.StatusOK, &getLog{expectedLog})
+
+	log := getLogsClient(requestMatcher)
+
+	returnedLog, err := log.GetLog(expectedLog.Id)
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedLog, returnedLog)
+}
+
+func TestLogs_GetLogErrorsIfLogSetIdIsEmpty(t *testing.T) {
+	log := Logs{nil}
+	_, err := log.GetLog("")
+	assert.NotNil(t, err)
+	assert.Error(t, err, "logId input parameter is mandatory")
+}
+
+func TestLogs_PostLog(t *testing.T) {
+
+	p := PostLog{
+			Name:        "My New Awesome Log",
+			Structures: []string{},
+			SourceType: "token",
+			LogsetsInfo: []PostLogSetInfo{
+				{"log-uuid"},
+			},
+			UserData: LogUserData{
+				LogEntriesAgentFileName:"",
+				LogEntriesAgentFollow:"false",
+			},
+		}
+
+	expectedLog := Log{
+		Id:         "log-set-uuid",
+		Name:       p.Name,
+		Tokens:     []string {"daf42867-a82f-487e-95b7-8d10dba6c4f5"},
+		Structures: []string{},
+		LogsetsInfo: []LogInfo {
+			{Id: p.LogsetsInfo[0].Id},
+		},
+		UserData: LogUserData{
+			LogEntriesAgentFileName: p.UserData.LogEntriesAgentFileName,
+			LogEntriesAgentFollow:   p.UserData.LogEntriesAgentFollow,
+		},
+	}
+
+	requestMatcher := testutils.NewRequestMatcher(http.MethodPost, "/management/logs", &postLog{p}, http.StatusCreated, &getLog{expectedLog})
+	log := getLogsClient(requestMatcher)
+
+	returnedLog, err := log.PostLog(p)
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedLog, returnedLog)
+
+}
+
+func TestLogs_PutLog(t *testing.T) {
+
+	logId := "log-set-uuid"
+
+	p := PutLog{
+		Name:        "My New Awesome Log",
+		Structures: []string{},
+		SourceType: "token",
+		LogsetsInfo: []LogSetInfo{
+			{
+				Id:"log-set-uuid",
+				Name: "ibtest",
+				Links: []Link {
+					{
+						Href: "https://rest.logentries.com/management/logsets/log-set-uuid",
+						Rel:"Self",
+					},
+				},
+			},
+		},
+		UserData: LogUserData{
+			LogEntriesAgentFileName:"",
+			LogEntriesAgentFollow:"false",
+		},
+	}
+
+	expectedLog := Log{
+		Id:         logId,
+		Name:       p.Name,
+		Tokens:     []string {"daf42867-a82f-487e-95b7-8d10dba6c4f5"},
+		Structures: []string{},
+		LogsetsInfo: []LogInfo {
+			{
+				Id: p.LogsetsInfo[0].Id,
+				Name: p.LogsetsInfo[0].Name,
+				Links: []Link {
+					{
+						Href: p.LogsetsInfo[0].Links[0].Href,
+						Rel:p.LogsetsInfo[0].Links[0].Rel,
+					},
+				},
+			},
+		},
+		UserData: LogUserData{
+			LogEntriesAgentFileName: p.UserData.LogEntriesAgentFileName,
+			LogEntriesAgentFollow:   p.UserData.LogEntriesAgentFollow,
+		},
+	}
+
+	url := fmt.Sprintf("/management/logs/%s", logId)
+	requestMatcher := testutils.NewRequestMatcher(http.MethodPut, url, &putLog{p}, http.StatusOK, &getLog{expectedLog})
+	log := getLogsClient(requestMatcher)
+
+	returnedLog, err := log.PutLog(logId, p)
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedLog, returnedLog)
+
+}
+
+func TestLogs_PutLogErrorsIfLogSetIdIsEmpty(t *testing.T) {
+	log := Logs{nil}
+	_, err := log.PutLog("", PutLog{})
+	assert.NotNil(t, err)
+	assert.Error(t, err, "logId input parameter is mandatory")
+}
+
+func TestLogs_DeleteLog(t *testing.T) {
+	logId := "log-set-uuid"
+
+	url := fmt.Sprintf("/management/logs/%s", logId)
+	requestMatcher := testutils.NewRequestMatcher(http.MethodDelete, url, nil, http.StatusNoContent, nil)
+	log := getLogsClient(requestMatcher)
+
+	err := log.DeleteLog(logId)
+	assert.Nil(t, err)
+}
+
+func TestLogs_DeleteLogErrorsIfLogSetIdIsEmpty(t *testing.T) {
+	log := Logs{nil}
+	err := log.DeleteLog("")
+	assert.NotNil(t, err)
+	assert.Error(t, err, "logId input parameter is mandatory")
 }
 
 func getLogsClient(requestMatcher testutils.TestRequestMatcher) Logs {
