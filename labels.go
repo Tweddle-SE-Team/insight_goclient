e79b3c1cb916c62acf4ad84e401f11288c990272
Daniel Isaac Khan Ramiro <di.khan.r@gmail.com>
2017-09-24 11:52:19 -0700

    Create labels resource

---
+++ labels.go
@@ -0,0 +1,76 @@
+package logentries_goclient
+
+import (
+	"errors"
+	"fmt"
+)
+
+// The Labels resource allows you to interact with Labels in your account. The following operations are supported:
+// - Get details of an existing label
+// - Get details of a list of all labels
+
+// Labels represents the label interface by which user can interact with logentries labels API
+type Labels struct {
+	client *client `json:"-"`
+}
+
+// NewLabels creates a new Labels struct that exposes Labels CRUD operations
+func NewLabels(c *client) Labels {
+	return Labels{c}
+}
+
+// Structs meant for clients
+
+// Label represents the entity used to get an existing label from the logentries API
+type Label struct {
+	Id       string `json:"id"`
+	SN       int    `json:"sn"`
+	Name     string `json:"name"`
+	Color    string `json:"color"`
+	Reserved bool   `json:"reserved"`
+}
+
+type getLabels []Label
+
+// Structs meant for marshalling/un-marshalling purposes
+
+// labelsCollection represents a wrapper struct for marshalling/unmarshalling purposes
+type labelsCollection struct {
+	Labels []Label `json:"labels"`
+}
+
+// getLabel represents a wrapper struct for marshalling/unmarshalling purposes
+type getLabel struct {
+	Label Label `json:"label"`
+}
+
+// GetLabels gets details of a list of all Labels
+func (l *Labels) GetLabels() ([]Label, error) {
+	labels := &labelsCollection{}
+	if err := l.client.get(l.getPath(), labels); err != nil {
+		return nil, err
+	}
+	return labels.Labels, nil
+}
+
+// GetLabel gets details of an existing Label
+func (l *Labels) GetLabel(labelId string) (Label, error) {
+	if labelId == "" {
+		return Label{}, errors.New("labelId input parameter is mandatory")
+	}
+	label := &getLabel{}
+	if err := l.client.get(l.getLabelEndPoint(labelId), label); err != nil {
+		return Label{}, err
+	}
+	return label.Label, nil
+}
+
+// getPath returns the rest end point for labels
+func (l *Labels) getPath() string {
+	return "/management/labels"
+}
+
+// getLabelEndPoint returns the rest end point to retrieve an individual labels
+func (l *Labels) getLabelEndPoint(labelId string) string {
+	return fmt.Sprintf("%s/%s", l.getPath(), labelId)
+}
---
+++ labels_test.go
@@ -0,0 +1,63 @@
+package logentries_goclient
+
+import (
+	"testing"
+	"github.com/dikhan/logentries_goclient/testutils"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"fmt"
+	"net/http"
+)
+
+func TestLabels_GetLabels(t *testing.T) {
+
+	expectedLabels := []Label{
+		{
+			Id:       "label-uuid",
+			Name:     "Login Failure",
+			Reserved: false,
+			Color:    "007afb",
+			SN:       1056,
+		},
+	}
+
+	requestMatcher := testutils.NewRequestMatcher(http.MethodGet, "/management/labels", nil, http.StatusOK, &labelsCollection{expectedLabels})
+	labels := getLabelsClient(requestMatcher)
+
+	returnedLabels, err := labels.GetLabels()
+	assert.Nil(t, err)
+	assert.True(t, reflect.DeepEqual(expectedLabels, returnedLabels))
+}
+
+func TestTags_GetLabel(t *testing.T) {
+
+	expectedLabel := Label{
+			Id:       "label-uuid",
+			Name:     "Login Failure",
+			Reserved: false,
+			Color:    "007afb",
+			SN:       1056,
+	}
+
+	url := fmt.Sprintf("/management/labels/%s", expectedLabel.Id)
+	requestMatcher := testutils.NewRequestMatcher(http.MethodGet, url, nil, http.StatusOK, &getLabel{expectedLabel})
+
+	labels := getLabelsClient(requestMatcher)
+
+	returnedLabel, err := labels.GetLabel(expectedLabel.Id)
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedLabel, returnedLabel)
+
+}
+
+func TestTags_GetLabelErrorsIfTagIdIsEmpty(t *testing.T) {
+	labels := Labels{nil}
+	_, err := labels.GetLabel("")
+	assert.NotNil(t, err)
+	assert.Error(t, err, "labelId input parameter is mandatory")
+}
+
+func getLabelsClient(requestMatcher testutils.TestRequestMatcher) Labels {
+	c := getTestClient(requestMatcher)
+	return NewLabels(c)
+}
