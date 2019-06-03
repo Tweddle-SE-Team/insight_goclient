482b7ab69af8db3f625851be3bae1e00a2638698
Andrew Chubatiuk <achubatiuk@Andrews-MacBook-Pro.local>
2019-06-03 17:06:52 +0300

    fixed request params

--- target.go
+++ target.go
@@ -18,9 +18,9 @@ type Target struct {
 	Id              string                 `json:"id,omitempty"`
 	Type            string                 `json:"type"`
 	Name            string                 `json:"name"`
-	Description     string                 `json:"description"`
+	Description     string                 `json:"description,omitempty"`
 	ParameterSet    *TargetParameterSet    `json:"params_set"`
-	UserData        map[string]string      `json:"user_data"`
+	UserData        map[string]string      `json:"user_data,omitempty"`
 	AlertContentSet *TargetAlertContentSet `json:"alert_content_set"`
 }
 
